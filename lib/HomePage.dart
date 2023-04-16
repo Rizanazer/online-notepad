a58493a1d13bf70b51bbb53deb4ed54eae66bf06
Rizanazer <ichurizan123@gmail.com>
2023-04-16 22:25:37 +0530

    2nd commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:online_note/getusername.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +11,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // ignore: non_constant_identifier_names
+  List<String> DocId = [];
+  Future getDocId() async {
+    await FirebaseFirestore.instance
+        .collection("user")
+        .get()
+        // ignore: avoid_function_literals_in_foreach_calls
+        .then((snapshat) => snapshat.docs.forEach((document) {
+              // ignore: avoid_print
+              print(document.reference);
+              DocId.add(document.reference.id);
+            }));
+  }
+
   final user = FirebaseAuth.instance.currentUser!;
   Future signout() async {
     await FirebaseAuth.instance.signOut();
@@ -18,16 +34,39 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Center(
+          child: Text(user.email!),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              signout();
+            },
+            child: const Icon(Icons.logout),
+          )
+        ],
+      ),
       body: SafeArea(
         child: Column(children: [
-          Center(
-            child: Text(user.email!),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                signout();
-              },
-              child: const Text("signout"))
+          Expanded(
+              child: FutureBuilder(
+            future: getDocId(),
+            builder: (context, snapshot) {
+              return ListView.builder(
+                  itemCount: DocId.length,
+                  // ignore: avoid_types_as_parameter_names, non_constant_identifier_names
+                  itemBuilder: (context, Index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        tileColor: Colors.blue[100],
+                        title: GetUserData(documentId: DocId[Index]),
+                      ),
+                    );
+                  });
+            },
+          ))
         ]),
       ),
     );
